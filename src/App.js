29ebac33086a21573718dc9797dccc4fbbb76d76
Umera.Z <cryptokukduku@gmail.com>
2023-06-18 16:31:18 +0530

    Mern Dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Products from './components/Products';
+import AddProduct from './components/AddProduct';
+import Logout from './components/Logout';
+import Profile from './components/Profile';
+import UpdateProduct from './components/UpdateProduct';
+import Footer from './Footer';
+import SignUp from './SignUp';
+import Login from "./Login"
+import PrivateComponent from "./components/PrivateComponent";
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route element={<PrivateComponent/>}>
+        <Route path='/' element={<Products/>} />
+        <Route path='/add' element={<AddProduct/>} />
+        <Route path='/update/:id' element={<UpdateProduct/>} />
+        <Route path='/logout' element={<Logout/>} />
+        <Route path='/profile/:id' element={<Profile/>} />
+      </Route>
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+      </BrowserRouter>
+        <Footer/>
     </div>
+    </>
   );
 }
 
